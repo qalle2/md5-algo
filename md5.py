13d3c4b2b9047d734ef3cc6a5484e349ab4acc6d
Kalle <qalle85@gmail.com>
2019-12-18 20:50:13 +0200

    tidy up; make pylint happy

--- md5.py
+++ md5.py
@@ -1,40 +1,24 @@
-import binascii
+"""An implementation of the MD5 algorithm."""
+
 import math
 import struct
 import sys
 
-# MD5 constants
-INITIAL_STATE = (0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476)
-BYTES_PER_CHUNK = 64
-ORIGINAL_SIZE_BYTE_COUNT = 8  # bytes needed for original message size
-PADDING_BYTE = b"\x00"
+def prepare_message(message):
+    """Prepare message for hashing."""
 
-def _prepare_message(message):
-    originalLen = len(message)
-    message = bytearray(message)
+    # append terminator byte, padding and original length in bits modulo 2 ** 64; the new length
+    # will be a multiple of 64 bytes
+    paddingSize = (64 - 1 - 8 - len(message) % 64) % 64
+    lengthInBits = (len(message) * 8) % 2 ** 64
+    return message + b"\x80" + paddingSize * b"\x00" + struct.pack("<Q", lengthInBits)
 
-    # append a "1" bit (and seven "0" bits)
-    message.append(0b1000_0000)
+def rotate_left(n, amount):
+    """Rotate a 32-bit integer left."""
 
-    # append "\x00" bytes to make
-    #     length_in_bytes + ORIGINAL_SIZE_BYTE_COUNT
-    # a multiple of BYTES_PER_CHUNK
-    lenRemainder = len(message) % BYTES_PER_CHUNK
-    paddingSize = (-ORIGINAL_SIZE_BYTE_COUNT - lenRemainder) % BYTES_PER_CHUNK
-    message += paddingSize * PADDING_BYTE
+    return ((n << amount) & 0xffff_ffff) | (n >> (32 - amount))
 
-    # append original length in bits modulo 2**64 (eight little-endian bytes)
-    originalLenInBits = (originalLen * 8) % 2**64
-    return message + struct.pack("<Q", originalLenInBits)
-
-def _generate_chunks(message):
-    """Prepare the message and generate it in chunks of 16 unsigned 32-bit
-    little-endian integers (64 bytes, 512 bits)."""
-
-    for chunk in struct.iter_unpack("<16I", _prepare_message(message)):
-        yield chunk
-
-def _hash_chunk(state, chunk):
+def hash_chunk(state, chunk):
     """Hash one chunk.
     state: 4 unsigned 32-bit integers
     chunk: 16 unsigned 32-bit integers
@@ -43,8 +27,6 @@ def _hash_chunk(state, chunk):
     (a, b, c, d) = state
 
     for i in range(64):
-        const = math.floor(abs(math.sin(i + 1)) * 2**32)
-
         if i < 16:
             bits = (b & c) | (~b & d)
             index = i
@@ -62,52 +44,49 @@ def _hash_chunk(state, chunk):
             index = 7 * i % 16
             shift = (6, 10, 15, 21)[i % 4]
 
+        const = math.floor(abs(math.sin(i + 1)) * 2 ** 32)
         bAdd = (const + a + bits + chunk[index]) & 0xffff_ffff
-        bAdd = ((bAdd << shift) & 0xffff_ffff) | (bAdd >> (32 - shift))
+        bAdd = rotate_left(bAdd, shift)
 
-        a = d
-        d = c
-        c = b
-        b = (b + bAdd) & 0xffff_ffff
+        (a, b, c, d) = (d, (b + bAdd) & 0xffff_ffff, b, c)
 
     return (a, b, c, d)
 
-def hash(message):
-    """Hash a string of bytes. Return the hash as 16 bytes. See references."""
+def md5(message):
+    """Hash a bytestring. Return the hash as 16 bytes. See references."""
 
-    state = list(INITIAL_STATE)
-    for chunk in _generate_chunks(message):
-        hash = _hash_chunk(state, chunk)
-        state = [(s + h) & 0xffff_ffff for (s, h) in zip(state, hash)]
+    # set initial state
+    state = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]
+    # prepare message and process it in chunks of 64 bytes (16 32-bit integers)
+    for chunk in struct.iter_unpack("<16I", prepare_message(message)):
+        # hash the chunk; add each 32-bit integer to the corresponding integer in the state
+        hash_ = hash_chunk(state, chunk)
+        state = [(s + h) & 0xffff_ffff for (s, h) in zip(state, hash_)]
+    # the final state is the hash
     return b"".join(struct.pack("<I", number) for number in state)
 
-def hexadecimal_hash(message):
-    """Hash a string of bytes. Return the hash in hexadecimal."""
-
-    return binascii.hexlify(hash(message)).decode("ascii")
-
 def main():
-    if len(sys.argv) != 2:
-        exit("Error: invalid number of arguments. See the readme file.")
+    """The main function."""
 
+    if len(sys.argv) != 2:
+        sys.exit("Error: invalid number of arguments.")
     message = sys.argv[1]
     try:
-        message = binascii.unhexlify(message)
-    except binascii.Error:
-        exit("Error: invalid argument. See the readme file.")
+        message = bytes.fromhex(message)
+    except ValueError:
+        sys.exit("Error: invalid argument.")
+    print(md5(message).hex())
 
-    print(hexadecimal_hash(message))
-
-assert hexadecimal_hash(b"")       == "d41d8cd98f00b204e9800998ecf8427e"
-assert hexadecimal_hash(b"\x00")   == "93b885adfe0da089cdf634904fd59f71"
-assert hexadecimal_hash(b"\xff")   == "00594fd4f42ba43fc1ca0427a0576295"
-assert hexadecimal_hash(b"ximaz")  == "61529519452809720693702583126814"
-assert hexadecimal_hash(55 * b"a") == "ef1772b6dff9a122358552954ad0df65"
-assert hexadecimal_hash(56 * b"a") == "3b0c8ac703f828b04c6c197006d17218"
-assert hexadecimal_hash(57 * b"a") == "652b906d60af96844ebd21b674f35e93"
-assert hexadecimal_hash(63 * b"a") == "b06521f39153d618550606be297466d5"
-assert hexadecimal_hash(64 * b"a") == "014842d480b571495a4a0363793f7367"
-assert hexadecimal_hash(65 * b"a") == "c743a45e0d2e6a95cb859adae0248435"
+assert md5(b"").hex() == "d41d8cd98f00b204e9800998ecf8427e"
+assert md5(b"\x00").hex() == "93b885adfe0da089cdf634904fd59f71"
+assert md5(b"\xff").hex() == "00594fd4f42ba43fc1ca0427a0576295"
+assert md5(b"ximaz").hex() == "61529519452809720693702583126814"
+assert md5(55 * b"a").hex() == "ef1772b6dff9a122358552954ad0df65"
+assert md5(56 * b"a").hex() == "3b0c8ac703f828b04c6c197006d17218"
+assert md5(57 * b"a").hex() == "652b906d60af96844ebd21b674f35e93"
+assert md5(63 * b"a").hex() == "b06521f39153d618550606be297466d5"
+assert md5(64 * b"a").hex() == "014842d480b571495a4a0363793f7367"
+assert md5(65 * b"a").hex() == "c743a45e0d2e6a95cb859adae0248435"
 
 if __name__ == "__main__":
     main()
